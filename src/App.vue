868f35d19287c2e7c04a43f495c8435bd86e361d
lixin1sj <lixin1sj@yonyou.com>
2019-03-31 14:17:44 +0800

    项目资源准备（css、js、styl以及字体文件）

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
+
 export default {
-  name: 'App'
+  components: {
+    MHeader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
 </style>
